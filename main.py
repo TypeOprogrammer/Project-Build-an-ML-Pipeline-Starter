d50a0a2c830d0321819f0cfc380c14932f998d88
TypeOprogrammer <logancausey@hotmail.com>
2025-01-04 14:09:04 -0500

    added data_split logic to main.py

--- main.py
+++ main.py
@@ -87,10 +87,22 @@ def go(config: DictConfig):
             #pass
 
         if "data_split" in active_steps:
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                'main',
+                parameters = {
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config['modeling']['test_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "stratify_by": config['modeling']['stratify_by']
+                },
+
+            )
+            
             ##################
             # Implement here #
             ##################
-            pass
+            #pass
 
         if "train_random_forest" in active_steps:
 
